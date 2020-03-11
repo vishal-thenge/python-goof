c8e44437eb268d7c2efda46c0562153c6444fc22
Matt @ Snyk <59509697+snyk-matt@users.noreply.github.com>
2020-03-11 12:49:08 -0400

    Update GithubTest.py

--- GithubTest.py
+++ GithubTest.py
@@ -1,7 +1,6 @@
 from github import Github
 
 g = Github("1bd500a3face4b51a9a88f1e92626a5467c226fe")
-GIT_OBJECT = Github(login_or_token=g)
 
 for repo in g.get_user().get_repos():
     print(repo.name)
